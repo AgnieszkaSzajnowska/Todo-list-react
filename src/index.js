1289066f43e5c5b429a702034fbcae5c1805a75a
Agnieszka Szajnowska <agnieszka.badyla@o2.pl>
2025-04-11 07:37:14 +0200

    Conteiner moved from folder

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import { GlobalStyle } from "./GlobalStyle";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from "./theme";
+import { Normalize } from "styled-normalize";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Normalize />
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
