f4d19ad3d78651694d87da0c66c414432a3b3b1e
Agnieszka Szajnowska <agnieszka.badyla@o2.pl>
2025-04-25 08:49:33 +0200

    Navigation added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/author/AuthorPage";
+import Navigation from "./common/Navigation";
+import { toAuthor, toTasks, toTask } from "./routes";
 
-const App = () => <Tasks />;
+const App = () => (
+  <HashRouter>
+    <Navigation />
+    <Switch>
+      <Route path={toTask()}>
+        <TaskPage />
+      </Route>
+      <Route path={toTasks()}>
+        <TasksPage />
+      </Route>
+      <Route path={toAuthor()}>
+        <AuthorPage />
+      </Route>
+      <Redirect to={toTasks()} />
+    </Switch>
+  </HashRouter>
+);
 
 export default App;
